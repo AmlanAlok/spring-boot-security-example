e5bc9fcae23c15c4dfac4650a1fc08b272e1bc45
Amlan Alok <amlanalok@gmail.com>
2024-02-05 17:59:18 -0600

    im memory authentication implemented

--- src/main/java/com/amlanalok/springbootsecurityexample/config/SecurityConfiguration.java
+++ src/main/java/com/amlanalok/springbootsecurityexample/config/SecurityConfiguration.java
@@ -1,24 +1,44 @@
 package com.amlanalok.springbootsecurityexample.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .withUser("amlan")
+                .password(passwordEncoder().encode("alok"))
+                .authorities("USER", "ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().permitAll(); // disables all security
+        http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin();
+    }
 }
-
-
-
-
-
 
 /**
 
- Default implementation of spring security below:
+ --> Default implementation of spring security below:
 
 protected void configure(HttpSecurity http) throws Exception {
     this.logger.debug("Using default configure(HttpSecurity). "
